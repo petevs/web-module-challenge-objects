a496f3537a80f9b67d46ab2b3b507e426530c14d
Pete VS <pete_vs88@me.com>
2020-08-25 20:07:36 -0400

    completed task 2

--- index.js
+++ index.js
@@ -36,6 +36,16 @@ and should return a number.
 
 For example, burger.discount("teacher") would return 13.5 and burger.discount("public") would return 16.2*/
 
+function discount(role) {
+  if (role === "public") {
+    return this.price * 0.9;
+  } else if (role === "teacher" || "student") {
+    return this.price * 0.75;
+  }
+}
+
+burger.discount = discount;
+
 ///////////////Reviews (MVP)///////////////////
 
 const reviews = [
