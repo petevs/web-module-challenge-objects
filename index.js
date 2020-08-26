acd15d943a1ee6a88794c3de3901ef25b6d99972
Pete VS <pete_vs88@me.com>
2020-08-25 20:50:01 -0400

    completed task 7

--- index.js
+++ index.js
@@ -138,8 +138,10 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
+function getLastReview(reviews) {
   /* code here */
+  let latest = reviews[reviews.length - 1];
+  return `${latest.name} gave the restaurant a ${latest.rating} star rating and their feedback was: \"${latest.feedback}\"`;
 }
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
